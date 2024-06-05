f15d99ecc6a6169dcf6de456b57376b0ad5447f1
dslee <leedoseu@naver.com>
2024-06-05 21:41:30 +0900

    first commit

---
+++ src/main/java/com/cicd/Controller/indexController.java
@@ -0,0 +1,15 @@
+package com.cicd.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/hello";
+    }
+
+}
---
+++ src/main/java/com/cicd/Hello.java
@@ -0,0 +1,12 @@
+package com.cicd;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Hello {
+
+    private String data;
+
+}
